a1389f588ef5dd303ab032bd61126870d17ac76b
farukmeral25 <farukmeral25@gmail.com>
2022-02-21 15:40:00 +0300

    Initial Commit

---
+++ random_photo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  random_photo
+//
+//  Created by Ömer Faruk Meral on 21.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
